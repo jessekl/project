06421f1bb0b8b3c9011ddc367ddfdc5c3e9654ee
Jesse Lurie <jesselurie@wg-dhcp186d219d247.win.wireless.fsu.edu>
2014-09-26 14:44:19 -0400

    added home page

---
+++ home.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>home</title>
+</head>
+<body>
+	<?php
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+	$repeatpassword = $_POST["repeatpassword"];
+	$candy = $_POST["candy"] ;
+	echo $email;
+	echo $password;
+	echo $repeatpassword;
+	echo $candy;
+	?>
+
+</body>
+</html>
